f1a7c3d1e332a9bb20a10726dc87be33d90d9041
murad <murad9@users.noreply.github.com>
2021-02-24 17:22:21 -0600

    first commit

---
+++ microservice-user-management/src/test/java/com/mur/microserviceusermanagement/MicroserviceUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mur.microserviceusermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
